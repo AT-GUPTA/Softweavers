737176652f1dbac6815978478ca60aae83f5dbbb
Alexander Figueiras <Figueiras.Alexander@gmail.com>
2023-03-23 22:41:44 -0400

    Added javadoc style commend for Standard Deviation

--- Source/api/eternity/src/main/java/com/softweavers/eternity/Domain/FunctionHandler.java
+++ Source/api/eternity/src/main/java/com/softweavers/eternity/Domain/FunctionHandler.java
@@ -15,5 +15,13 @@ public interface FunctionHandler {
 
     BigDecimal xToY(BigDecimal y);
 
+    /**
+     * First calculates the mean of the given inputs.
+     * For each value it then subtracts the mean and squares that number.
+     * Then divides by the total number of inputs and finishes by
+     * taking the square root by raising that number to the power of a half.
+     * @param input array of BigDecimal numbers that will be used to calculate the standard deviation
+     * @return the standard deviation of the given inputs
+     */
     BigDecimal standardDeviation(BigDecimal[] input);
 }
